2af2767a4b12b6a7badf583a20a65374a668b420
KooBonchan <garam9koo@kaist.ac.kr>
2019-05-26 23:34:22 +0900

    temp-commit

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // http get room - returns restaurant name, min price, curr-price, orders(name, dish name, price), dorm
+    let room_info = {};
+    let chat_ls = [];
+    
+    // http get chat(room, curr-time - 1 day) - load chatlist(name, text, timestamp) after time
+    // load to doc
+})
+
+setInterval(()=>{
+    // http get room/list - returns roomlist(restaurant name, owner name, currprice, minprice)
+    
+}, 5000);
